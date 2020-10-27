5b64471658c57b909247fde748c66510e84b08a2
rednaxela700 <ohleg@windowslive.com>
2020-10-27 16:19:18 +0100

    src/components/Dashboard.js: moved PieChart data fetch to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PieChartComponent from "./PieChart";
-import { Grid } from "semantic-ui-react";
+import { Grid, Loader } from "semantic-ui-react";
 import DataBarChart from "./DataChart";
+import { fetchTrips } from "../services/TripService";
+
 
 function Dashboard() {
+  const [loading, setLoading] = useState(false)
+  const [pieChartData, setPieChartData] = useState([]);
+
+  useEffect(()=> {
+    setLoading(true)
+    handlePieChartData();
+    setLoading(false)
+    // eslint-disable-next-line
+  }, [])
+
+
+  const handlePieChartData = async () => {
+    const continentsColors = ['#0088FE', '#00C49F', '#FFBB28', '#d37736', '#FF8042', '#ff3c42', '#764afe'];
+
+    const result = await fetchTrips()
+    const distribution = result.reduce((result, next) => {
+      result[next.continent] = (result[next.continent] || 0) + 1
+      return result;
+    }, {})
+    const trips = Object.entries(distribution).map(([name, value]) => ({ name, value }))
+    const continentsWithColors = trips.map((continent, index) => ({ ...continent, color: continentsColors[index] }))
+    setPieChartData(continentsWithColors)
+
+  }
+  if(loading) return <Loader/>
   return (
     <div className="Dashboard">
       <Grid padded={true}>
@@ -14,7 +41,7 @@ function Dashboard() {
         </Grid.Row>
         <Grid.Row>
           <Grid.Column widescreen={8} largeScreen={8} mobile={16}>
-            <PieChartComponent />
+            <PieChartComponent data={pieChartData} loading={loading} />
           </Grid.Column>
           <Grid.Column widescreen={8} largeScreen={8} mobile={16} verticalAlign={'middle'}>
             <DataBarChart />
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   PieChart,
   Pie,
   Cell,
   Tooltip
 } from 'recharts';
-import { fetchTrips } from "../services/TripService";
 
 const windowWidth = window.screen.width;
-const continentsColors = ['#0088FE', '#00C49F', '#FFBB28', '#d37736', '#FF8042', '#ff3c42', '#764afe'];
+const PieChartComponent = ({data, loading}) => {
 
-const PieChartComponent = () => {
-  const [tripsData, setTripsData] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const result = await fetchTrips()
-      const distribution = result.reduce((result, next) => {
-        result[next.continent] = (result[next.continent] || 0) + 1
-        return result;
-      }, {})
-      const trips = Object.entries(distribution).map(([name, value]) => ({ name, value }))
-      const continentsWithColors = trips.map((continent, index) =>  ({...continent, color: continentsColors[index]}))
-      setTripsData(continentsWithColors)
-    })();
-    //eslint-disable-next-line
-  }, [])
+  if(loading) return null
 
   return (<div>
     <PieChart
@@ -34,14 +18,14 @@ const PieChartComponent = () => {
       style={{ margin: '0 auto' }}
     >
       <Pie
-        data={tripsData}
+        data={data}
         labelLine={true}
         fill="#8884d8"
         dataKey="value"
         label
       >
         {
-          tripsData.map((entry, index) => <Cell key={`cell-${index}`} fill={entry.color} />)
+          data.map((entry, index) => <Cell key={`cell-${index}`} fill={entry.color} />)
         }
       </Pie>
       <Tooltip />
