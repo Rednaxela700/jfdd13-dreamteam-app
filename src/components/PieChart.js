3460324d339f6ce242002742ae1e7a11e357d85d
rednaxela700 <ohleg@windowslive.com>
2020-10-27 17:45:18 +0100

    src/components/PieChart.js: rendering label with continent name

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -3,14 +3,26 @@ import {
   PieChart,
   Pie,
   Cell,
-  Tooltip
+  Tooltip,
 } from 'recharts';
 
 const windowWidth = window.screen.width;
-const PieChartComponent = ({data, loading}) => {
+const PieChartComponent = ({ data, loading }) => {
 
-  if(loading) return null
+  if (loading) return null
+  const RADIAN = Math.PI / 180;
 
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, name }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5 + 100;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="#000" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${name}`}
+      </text>
+    );
+  };
   return (<div>
     <PieChart
       width={windowWidth > 500 ? 500 : 300}
@@ -22,10 +34,11 @@ const PieChartComponent = ({data, loading}) => {
         labelLine={true}
         fill="#8884d8"
         dataKey="value"
-        label
+        label={renderCustomizedLabel}
       >
         {
-          data.map((entry, index) => <Cell key={`cell-${index}`} fill={entry.color} />)
+          data.map((entry, index) => <Cell key={`cell-${entry.name}`} fill={entry.color} >
+          </Cell>)
         }
       </Pie>
       <Tooltip />
