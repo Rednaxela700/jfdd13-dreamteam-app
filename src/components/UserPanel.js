c08c9b8f20a15eeb427e282451836baf38eddde9
rednaxela700 <ohleg@windowslive.com>
2020-10-29 12:53:59 +0100

    src/components/UserPanel.js: created user card with default img

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import { Header, Card, Icon } from "semantic-ui-react";
+import { Container, Header, Card, Icon, Image } from "semantic-ui-react";
+import defaultAvatar from '../assets/icon.svg'
 
 function UserPanel({ data }) {
-  const { email, name, date } = data
 
+  const { email, name, date, bio } = data
+  let processedDate
+  if (date) {
+    processedDate = new Date(date).toLocaleDateString()
+  }
 
-  
   return (
-    <div>
-      <Header>dane z firebase dla {name} </Header>
-      <p>email: {email}</p>
-
-      <Card>
-        <Card.Content header='Elliot Baker'/>
-        <Card.Content meta='Friend'/>
-        <Card.Content description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."/>
-        <Card.Content extra>
-          <div>
-            <Icon name='user' />
-            <span>Registered: {date}</span>
-          </div>
-        </Card.Content>
-      </Card>
-    </div>
+    <Container style={{ marginTop: '50px' }}>
+      <Header>Your account</Header>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Card>
+          <Image src={defaultAvatar} wrapped ui={false} style={{}} />
+          <Card.Content header={name} />
+          <Card.Content meta={() => (
+            <div style={{ color: '#000', cursor: 'pointer' }}>
+              {email}
+            </div>
+          )} />
+          {bio &&
+            <Card.Content description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat." />
+          }
+          {processedDate && <Card.Content extra>
+            <div>
+              <Icon name='calendar alternate outline' />
+              <span>Registered: {processedDate}</span>
+            </div>
+          </Card.Content>
+          }
+        </Card>
+      </div>
+    </Container>
   )
 }
 
