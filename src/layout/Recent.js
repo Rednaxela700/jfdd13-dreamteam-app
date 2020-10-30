e7a068b744e86e97c0d79b0abb6c68b7d056fafa
rednaxela700 <ohleg@windowslive.com>
2020-10-30 14:11:19 +0100

    src/layout/Recent.js: slider working only vertical direction

--- src/layout/Recent.js
+++ src/layout/Recent.js
@@ -1,23 +1,41 @@
-import React, {Component} from 'react'
-import {Carousel} from 'react-responsive-carousel'
+import React, { Component } from 'react'
+import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-
+import img1 from '../assets/Bergen.jpg'
+import img2 from '../assets/Madrid.jpg'
+import img3 from '../assets/Thailand.jpg'
 export class DemoCarousel extends Component {
+  prepareImages(img) {
+    // const image = new Image(816,583)
+    const image = img
+    console.log(image)
+    return (
+      <img src={image}
+        // style={{ maxWidth: image.width, height: image.height }}
+        className="" alt="" />
+    )
+  }
   render() {
+
     return (
       <Carousel
+        autoPlay
         infiniteLoop
+        axis="vertical"
         centerMode
-        centerSlidePercentage={('centerSlidePercentage', 80, {})}
+        dynamicHeight={true}
+        centerSlidePercentage={('centerSlidePercentage', 60, {}, 'mainGroupId')}
+        className="carousel"
+        showThumbs={false}
       >
         <div>
-          <p className="">Legend 1</p>
+          {this.prepareImages(img1)}
         </div>
         <div>
-          <p className="">Legend 2</p>
+          {this.prepareImages(img2)}
         </div>
         <div>
-          <p className="">Legend 3</p>
+          {this.prepareImages(img3)}
         </div>
       </Carousel>
     );
