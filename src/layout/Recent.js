bf297ae2ca69f19d23817147fbfcd53a33c14ca2
rednaxela700 <ohleg@windowslive.com>
2020-10-30 12:03:35 +0100

    src/layout/Recent.js: showing slider without content

--- src/layout/Recent.js
+++ src/layout/Recent.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {Carousel} from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+export class DemoCarousel extends Component {
+  render() {
+    return (
+      <Carousel
+        infiniteLoop
+        centerMode
+        centerSlidePercentage={('centerSlidePercentage', 80, {})}
+      >
+        <div>
+          <p className="">Legend 1</p>
+        </div>
+        <div>
+          <p className="">Legend 2</p>
+        </div>
+        <div>
+          <p className="">Legend 3</p>
+        </div>
+      </Carousel>
+    );
+  }
+};
 
 export default function Recent() {
   return (
     <section className="recent">
       <h2 className="section__title recent__title">Recently Added</h2>
       <div className="recent__container">
-        karuzela
+        <DemoCarousel></DemoCarousel>
       </div>
     </section>
   )
 }
+
