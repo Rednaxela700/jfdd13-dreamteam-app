89f6d3283c3e55aa47656e2c242440f455c85e86
rednaxela700 <ohleg@windowslive.com>
2020-10-30 08:32:19 +0100

    src/components/Header.js: finished markup for desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import userIcon from '../assets/navIcon.svg'
 
 export default function Nav() {
   return (
     <header className="header">
       <div className="header__container">
-        <div className="logo"><p>where<span className="logo--accent">to</span></p></div>
+        <div className="logo">
+          <p>where<span className="logo--accent">to</span></p>
+        </div>
         <nav className="nav">
           <ul className="nav__container">
             <li className="nav__item">
@@ -18,7 +21,9 @@ export default function Nav() {
               <Link to="/" className="nav__link">About</Link>
             </li>
             <li className="nav__item">
-              <Link to="/" className="nav__link">ikona</Link>
+              <Link to="/" className="nav__link">
+                <img className="nav__icon" src={userIcon} alt="user icon" />
+              </Link>
             </li>
           </ul>
         </nav>
