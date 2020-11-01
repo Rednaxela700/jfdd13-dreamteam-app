92916cde460cd5e05fe29497e8d46038029fced8
rednaxela700 <ohleg@windowslive.com>
2020-11-01 19:55:06 +0100

    src/screens/register/Register.js: added logo

--- src/screens/register/Register.js
+++ src/screens/register/Register.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from "react";
 import { register } from "../../services/AuthService";
 import IconGoogle from '../../assets/googleIcon.svg'
-import RegisterImg from '../../assets/register.jpg'
+import Logo from "../../layout/Logo";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -22,57 +22,63 @@ const Register = () => {
   return (
     <Fragment>
       <main className="wrapper login">
-        <div className="login__container">
-          <h1 className="section__title login__title">
-            Create an account
+        <section className="login__container">
+          <div className="login__logo">
+            <Logo />
+          </div>
+          <div className="login__content">
+            <h1 className="section__title login__title">
+              Create an account
         </h1>
-          <form
-            className="login__form"
-            onSubmit={(e) => e.preventDefault()}
-          >
-            <div className="">
-              {/* <input
+            <form
+              className="login__form"
+              onSubmit={(e) => e.preventDefault()}
+            >
+              <div className="">
+                {/* <input
                 className="form__input login__input"
                 value={name}
                 type="text"
                 onChange={event => setName(event.target.value)}
                 placeholder="Imię"
               /> */}
-              <div className="">
-                <input
-                  className="form__input login__input"
-                  value={email}
-                  type="text"
-                  onChange={event => setEmail(event.target.value)}
-                  placeholder="E-mail"
-                />
-              </div>
-              <div className="">
-                <input
-                  className="form__input login__input"
-                  value={password}
-                  onChange={event => setPassword(event.target.value)}
-                  placeholder="Hasło"
-                  type="password"
-                />
-              </div>
-              <button
-                className="login__input login__submit cta"
-                onClick={() => registerError()}
-                type="submit"
-              >
-                Sign up
+                <div className="">
+                  <input
+                    className="form__input login__input"
+                    value={email}
+                    type="text"
+                    onChange={event => setEmail(event.target.value)}
+                    placeholder="E-mail"
+                  />
+                </div>
+                <div className="">
+                  <input
+                    className="form__input login__input"
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                    placeholder="Hasło"
+                    type="password"
+                  />
+                </div>
+                <button
+                  className="login__input login--submit cta"
+                  onClick={() => registerError()}
+                  type="submit"
+                >
+                  Sign up
+                </button>
+                <button
+                  className="login__input login--submit login__google cta"
+                  onClick={() => registerError()}
+                  type="submit"
+                >
+                  <img src={IconGoogle} alt="" />&nbsp;Sign up with Google
             </button>
-              <button
-                className="login__input login--submit login__google cta"
-                onClick={() => registerError()}
-                type="submit"
-              >
-                <img src={IconGoogle} alt="" />&nbsp;Sign up with Google
-            </button>
-            </div>
-          </form>
-        </div>
+              </div>
+            </form>
+          </div>
+
+        </section>
         <div className="login__container login__bg">
           {/* <img src={RegisterImg} alt="" className="login__bg"/> */}
         </div>
