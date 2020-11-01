d9388144678747281fa22899e6926b4b5ad8f600
rednaxela700 <ohleg@windowslive.com>
2020-11-01 18:17:31 +0100

    src/screens/register/Register.js: added basic leayout

--- src/screens/register/Register.js
+++ src/screens/register/Register.js
@@ -1,8 +1,7 @@
 import React, { Fragment, useState } from "react";
-import {
-  Message,
-} from "semantic-ui-react";
 import { register } from "../../services/AuthService";
+import IconGoogle from '../../assets/googleIcon.svg'
+import RegisterImg from '../../assets/register.jpg'
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -21,55 +20,67 @@ const Register = () => {
   };
 
   return (
-      <Fragment>
-        <h1>
-          Utwórz konto
+    <Fragment>
+      <main className="wrapper login">
+        <div className="login__container">
+          <h1 className="section__title login__title">
+            Create an account
         </h1>
-        <form >
-          <div>
-            <input
-              value={name}
-              type="text"
-              onChange={event => setName(event.target.value)}
-              fluid
-              icon="user"
-              iconPosition="left"
-              placeholder="Imię"
-            />
-            <input
-              value={email}
-              type="text"
-              onChange={event => setEmail(event.target.value)}
-              fluid
-              icon="user"
-              iconPosition="left"
-              placeholder="E-mail"
-            />
-            <input
-              value={password}
-              onChange={event => setPassword(event.target.value)}
-              fluid
-              icon="lock"
-              iconPosition="left"
-              placeholder="Hasło"
-              type="password"
-            />
-
-            <button
-              onClick={() => registerError()}
-              color="blue"
-              fluid
-              size="large"
-              type="submit"
-            >
-              Zarejestruj się
+          <form
+            className="login__form"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <div className="">
+              {/* <input
+                className="form__input login__input"
+                value={name}
+                type="text"
+                onChange={event => setName(event.target.value)}
+                placeholder="Imię"
+              /> */}
+              <div className="">
+                <input
+                  className="form__input login__input"
+                  value={email}
+                  type="text"
+                  onChange={event => setEmail(event.target.value)}
+                  placeholder="E-mail"
+                />
+              </div>
+              <div className="">
+                <input
+                  className="form__input login__input"
+                  value={password}
+                  onChange={event => setPassword(event.target.value)}
+                  placeholder="Hasło"
+                  type="password"
+                />
+              </div>
+              <button
+                className="login__input login__submit cta"
+                onClick={() => registerError()}
+                type="submit"
+              >
+                Sign up
             </button>
-          </div>
-        </form>
-        {registerErrorMsg && (
-          <Message error={true}>Podaj poprawny E-mail oraz Hasło</Message>
-        )}
-      </Fragment>
+              <button
+                className="login__input login--submit login__google cta"
+                onClick={() => registerError()}
+                type="submit"
+              >
+                <img src={IconGoogle} alt="" />&nbsp;Sign up with Google
+            </button>
+            </div>
+          </form>
+        </div>
+        <div className="login__container login__bg">
+          {/* <img src={RegisterImg} alt="" className="login__bg"/> */}
+        </div>
+      </main>
+      {registerErrorMsg && (
+        <div className="form__error">Podaj poprawny E-mail oraz Hasło</div>
+      )}
+    </Fragment>
   );
 };
 
