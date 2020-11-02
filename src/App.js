3c78c3b31e653fa04e526028896c350a3f5de0b5
rednaxela700 <ohleg@windowslive.com>
2020-11-02 02:10:05 +0100

    src/App.js: deleted unused components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
-import Navbar from "./layout/Navbar";
 import Dashboard from "./components/Dashboard";
 import TripForm from "./components/Form/Form";
 import Favorites from "./components/Favourites";
 import Search from "./components/Search";
-import Appbar from "./layout/Appbar";
 import UserPanel from './components/UserPanel';
-import Register from './screens/Register';
-import Login from './screens/Login';
 import Favicon from 'react-favicon';
 import firebase from 'firebase'
 import './styles/index.scss'
@@ -45,24 +41,23 @@ function App({ user }) {
     <>
       <Favicon url="../public/favicon.ico" />
       <BrowserRouter>
-            <Appbar />
-            <Switch>
-              <Route exact strict path="/" render={(props) => (
-                <Main {...props} userData={userData} logged={true}  />
-              )}
-              />
-              <Route exact strict path="/main" component={Dashboard} />
-              <Route exact strict path="/search" component={Search} />
-              <Route exact strict path="/panel" render={(props) => (
-                <UserPanel {...props} data={userData} avatarUrl={avatarUrl} setAvatarUrl={setAvatarUrl} />
-              )} />
-              {/* <Route exact strict path="/register" component={Register} /> */}
-              {/* <Route exact strict path="/login" component={Login} /> */}
-              {/* <Route exact strict path="/user" component={LoggedUser} /> */}
-              <Route exact strict path="/form" component={TripForm} />
-              <Route exact strict path="/favs" component={Favorites} />
-              <Route component={() => <h1>404 - sadface</h1>} />
-            </Switch>
+        <Switch>
+          <Route exact strict path="/" render={(props) => (
+            <Main {...props} userData={userData} logged={true} />
+          )}
+          />
+          <Route exact strict path="/main" component={Dashboard} />
+          <Route exact strict path="/search" component={Search} />
+          <Route exact strict path="/panel" render={(props) => (
+            <UserPanel {...props} data={userData} avatarUrl={avatarUrl} setAvatarUrl={setAvatarUrl} />
+          )} />
+          {/* <Route exact strict path="/user" component={LoggedUser} /> */}
+          <Route exact strict path="/form" component={TripForm} />
+          <Route exact strict path="/favs" component={Favorites} />
+          <Route component={() => <h1>404 -
+                <span role="img" aria-label="error emoji">🧐</span>
+          </h1>} />
+        </Switch>
         <Redirect to="/" />
       </BrowserRouter>
     </>
