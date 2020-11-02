e799593de78f1177ecfa513bf373de6194e6cde9
rednaxela700 <ohleg@windowslive.com>
2020-11-02 02:05:28 +0100

    src/App.js: removed unused components

--- src/App.js
+++ src/App.js
@@ -45,9 +45,6 @@ function App({ user }) {
     <>
       <Favicon url="../public/favicon.ico" />
       <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <main className={'main'}>
             <Appbar />
             <Switch>
               <Route exact strict path="/" render={(props) => (
@@ -59,15 +56,13 @@ function App({ user }) {
               <Route exact strict path="/panel" render={(props) => (
                 <UserPanel {...props} data={userData} avatarUrl={avatarUrl} setAvatarUrl={setAvatarUrl} />
               )} />
-              <Route exact strict path="/register" component={Register} />
-              <Route exact strict path="/login" component={Login} />
+              {/* <Route exact strict path="/register" component={Register} /> */}
+              {/* <Route exact strict path="/login" component={Login} /> */}
               {/* <Route exact strict path="/user" component={LoggedUser} /> */}
               <Route exact strict path="/form" component={TripForm} />
               <Route exact strict path="/favs" component={Favorites} />
               <Route component={() => <h1>404 - sadface</h1>} />
             </Switch>
-          </main>
-        </div>
         <Redirect to="/" />
       </BrowserRouter>
     </>
