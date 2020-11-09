ebfaefd64817ef34299939086e456be7cf8cb5df
rednaxela700 <ohleg@windowslive.com>
2020-11-09 23:06:09 +0100

    src/components/pages/About.js: rendering pie chart

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,48 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import Header from "../../layout/Header";
 import DataBarChart from "../DataChart";
 import PieChartComponent from "../PieChart";
-import '../../mockup'
+import {pieChart, users} from "../../mockup";
+
 
 export default function About({logged, userData, avatarUrl, setAvatarUrl}) {
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarchartData] = useState([]);
+
+  useEffect(()=> {
+    getPieChartData();
+  },[])
+
+  const getPieChartData = () => {
+    const continentsColors = ['#0088FE', '#00C49F', '#FFBB28', '#d37736', '#FF8042', '#ff3c42', '#764afe'];
+
+    const result = Object.entries(pieChart).map(entry => {
+      const [id, trip] = entry
+      return {
+        id,
+        ...trip
+      }
+    })
+    const distribution = result.reduce((result, next) => {
+      result[next.name] = (result[next.name] || 0) + 1
+      return result;
+    }, {})
+    const trips = Object.entries(distribution).map(([name, value]) => ({name, value}))
+    const continentsWithColors = trips.map((continent, index) => ({...continent, color: continentsColors[index]}))
+    setPieChartData(continentsWithColors)
+  }
+
+  const getBarChartData = () => {
+    const fetched = Object.entries(users).map(entry => {
+      const [a, b] = entry;
+      return {
+        a,
+        ...b
+      }
+    });
+    const usersWithDate = fetched.filter(({date}) => date)
+    setBarchartData(usersWithDate)
+  }
   return (
     <Fragment>
       <Header
@@ -16,8 +54,8 @@ export default function About({logged, userData, avatarUrl, setAvatarUrl}) {
       <main className="wrapper">
         <div>
           <h1>About Page</h1>
-          <DataBarChart/>
-          <PieChartComponent/>
+          {/*<DataBarChart data={barChartData}/>*/}
+          <PieChartComponent data={pieChartData}/>
         </div>
       </main>
     </Fragment>
