5230db115d8dbbd4dba429edb747313ae62c9fb9
rednaxela700 <ohleg@windowslive.com>
2020-11-13 18:00:30 +0100

    assets/: added icons for trip page

--- src/components/pages/Trip.js
+++ src/components/pages/Trip.js
@@ -1,19 +1,24 @@
 import React, {useState, useEffect, useContext, Fragment} from 'react'
 import AppContext from '../../context/app/AppContext'
 import Header from "../../layout/Header";
+import iconCalendar from '../../assets/iconCalendar.svg'
+import iconCash from '../../assets/iconCash.svg'
+import iconCompass from '../../assets/iconCompass.svg'
 
 export default function Trip({match}) {
   const [current, setCurrent] = useState(null);
   const appContext = useContext(AppContext);
   const {trips} = appContext;
   useEffect(() => {
-    setCurrent(trips.find(el => el.id = match.params.tripid))
+    setCurrent(trips.find(el => el.id === match.params.tripid))
     //eslint-disable-next-line
     return ()=> setCurrent(null)
   }, [])
   if (!current) return null
   const {city, price, title, description, date, tripImageUrl} = current
-  const heroBg = {backgroundImage: `url(${tripImageUrl})`}
+  const heroBg = {
+    background: `url(${tripImageUrl}) no-repeat center`,
+  }
   return (
     <Fragment>
       <Header/>
@@ -25,6 +30,21 @@ export default function Trip({match}) {
           >
 
           </header>
+          <div className="site-details__container">
+            <div className="site-details__categories">
+              <figure className="icon__container"><img src={iconCash} alt="" className="icon__item"/>
+                <figcaption className='icon__description'>From 60EUR</figcaption>
+              </figure>
+              <figure className="icon__container"><img src={iconCalendar} alt="" className="icon__item"/>
+                <figcaption className='icon__description'>{date}</figcaption>
+              </figure>
+              <figure className="icon__container"><img src={iconCash} alt="" className="icon__item"/>
+                <figcaption className='icon__description'>From 60EUR</figcaption>
+              </figure>
+            </div>
+            <p className="site-details__text">{description}</p>
+            
+          </div>
         </section>
       </main>
     </Fragment>
